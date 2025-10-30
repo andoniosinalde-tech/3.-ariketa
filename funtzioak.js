75308387e22824e57251a24b2d1e9bb0b121604b
andoniosinalde-tech <andoniosinalde@arizmendikide.net>
2025-10-30 08:56:08 +0100

    Add files via upload

---
+++ funtzioak.js
@@ -0,0 +1,48 @@
+// 1. Testu-Aldatzailea
+function aldatuTestua() {
+const parrafo = document.getElementById("testu_aldagarria");
+parrafo.textContent = "Testua aldatu da!";
+}
+
+// 2. Kolore Txandakatzailea (Estiloak JS-tik aplikatuz)
+function aldatuKolorea() {
+const p = document.getElementById("kolore_aldagarria");
+if (p.style.color === "red") {
+        p.style.color = "blue";
+    } else {
+        p.style.color = "red";
+    }
+}
+
+// 3. Elementuen Ezkutagailua (display propietatea aldatuz)
+function txandakatuEzkutatzea() {
+
+}
+
+// 4. Batuketa Kalkulagailua
+function batuketaEgin() {
+
+}
+
+// 5. Testu-Gehitzailea (Elementu berriak sortzea)
+function gehituElementua() {
+
+}
+
+// 6. Kontagailua (aldagai globalak erabiliz)
+// Oharra: klikKopurua HTML amaierako <script> blokean hasieratuta dago
+function kontatuKlik() {
+
+}
+
+// 7. Zenbaki Asmatzearen Jokoa
+// Oharra: zenbakiSekretua HTML amaierako <script> blokean hasieratuta dago
+function konprobatuZenbakia() {
+
+}
+
+
+// 8. Atzeko Planoaren Kolore-Aldatzailea (Random)
+function aldatuAtzekoPlanoarenKolorea() {
+
+}
